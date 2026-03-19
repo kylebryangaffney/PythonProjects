026f6240f27a609852b18066b8e1c7db166a9e43
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-03-19 10:36:18 -1000

    Fix for step 223

--- ClassAssignment/main.py
+++ ClassAssignment/main.py
@@ -1,17 +1,27 @@
+# The Base Class
+# Archetype for other child animals.
 class Animal:
-    name = "Unknown Name"
-    vertebrate = False
-    habitat = "water"
+    def __init__(self, name="Unknown Name", vertebrate=False, habitat="water"):
+        # Set the default values if none are supplied
+        self.name = name
+        self.vertebrate = vertebrate
+        self.habitat = habitat
 
+# Inheritance: Arachnid "is an" Animal
 class Arachnid(Animal):
     warm_blooded = False
     number_of_legs = 8
+    # We change the default habitat from "water" to "land".
     habitat = "land"
 
+# Inheritance: Bird "is an" Animal
 class Bird(Animal):
+    # Overriding parent traits, birds are vertebrates
     warm_blooded = True
     vertebrate = True
+    
+    # Adding new bird specific attributes
     number_of_legs = 2
     habitat = "land"
-    can_fly = True        # can be overridden for penguins 
+    can_fly = True        # Can be overridden for birds like Penguins
     has_feathers = True
