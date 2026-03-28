90ad61161777e5caedbe4fd3fb97ee334401ff49
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-03-27 15:03:34 -1000

    Update admin_console

--- DjangoProject/src/mainapp/mainapp/urls.py
+++ DjangoProject/src/mainapp/mainapp/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from .views import home
@@ -22,6 +23,7 @@ from .views import home
 urlpatterns = [
     path('', home, name="home"),
     path('admin/', admin.site.urls),
+     path('', include("products.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
